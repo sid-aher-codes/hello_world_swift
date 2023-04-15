01edbb9c711415906a64146667e71c2884a290c0
Sid Aher <sidaher14@gmail.com>
2023-04-15 08:16:56 -0400

    Initial Commit

---
+++ Hello_world_Swift/Hello_world_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_world_SwiftApp.swift
+//  Hello_world_Swift
+//
+//  Created by Sid Aher on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_world_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
