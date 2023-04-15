53f5253bb176b7ed888c19abd6b493a0b9d9fe19
Sid Aher <sidaher14@gmail.com>
2023-04-15 08:24:06 -0400

    hello world Sid

--- Hello_world_Swift/ContentView.swift
+++ Hello_world_Swift/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Swift hello world")
+            Text("Swift hello world Sid")
         }
         .padding()
     }
